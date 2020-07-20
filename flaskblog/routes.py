25d84a9ea759a895b74b4716a0d108f928ffb411
sanjeev <saramasa@syr.edu>
2020-07-20 13:52:17 -0400

    Layout Changes

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -23,7 +23,7 @@ def home():
 def latest():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).limit(5).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    return render_template('latest.html', posts=posts)
 
     
 
